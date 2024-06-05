579a8d5cb730408022f6e89852f787be3a69e215
simplymichael <simplymichaelorji@gmail.com>
2024-06-05 23:02:51 +0100

    test: add tests to cover config.reset()

--- src/config/config.spec.js
+++ src/config/config.spec.js
@@ -4,6 +4,21 @@ const { chai } = require("../helpers/test-helper");
 const config = require("./");
 
 let expect;
+const defaultDbConfig = {
+  default: "mongodb",
+  connections: {
+    mongodb: {
+      dsn      : "",
+      host     : "0.0.0.0",
+      port     : 27017,
+      username : "",
+      password : "",
+      dbName   : "",
+      debug    : false,
+      exitOnConnectFail: true,
+    },
+  },
+};
 
 before(async function() {
   expect = (await chai()).expect;
@@ -11,23 +26,9 @@ before(async function() {
 
 module.exports = {
   get() {
-    describe(".get(path, defaultValue)", function configDotGet_Spec() {
+    describe(".get(path[, defaultValue])", function configDotGet_Spec() {
       it("should return `path` config value if exists", function() {
-        expect(config.get("database")).to.deep.equal({
-          default: "mongodb",
-          connections: {
-            mongodb: {
-              dsn      : "",
-              host     : "0.0.0.0",
-              port     : 27017,
-              username : "",
-              password : "",
-              dbName   : "",
-              debug    : false,
-              exitOnConnectFail: true,
-            },
-          },
-        });
+        expect(config.get("database")).to.deep.equal(defaultDbConfig);
       });
 
       it("should return dot-separated `path` config value if exists", function() {
@@ -64,6 +65,73 @@ module.exports = {
         expect(config.get("event")).to.be.an("object");
         expect(config.get("event")).to.have.property("actor");
         expect(config.get("event.actor")).to.equal("user");
+      });
+    });
+  },
+
+  reset() {
+    describe(".reset([path])", function configDotReset_Spec() {
+      it("should reset entire config object if no `path` specified", function() {
+        expect(config.get("app.environment")).to.equal("test");
+        expect(config.get("app.timezone")).to.equal("UTC");
+        expect(config.get("database")).to.deep.equal(defaultDbConfig);
+
+        config.set("app.environment", "staging");
+        config.set("app.timezone", "Africa/Lagos");
+        config.set("database.default", "mysql");
+
+        expect(config.get("app.environment")).to.equal("staging");
+        expect(config.get("app.timezone")).to.equal("Africa/Lagos");
+        expect(config.get("database")).to.deep.equal({
+          ...defaultDbConfig, default: "mysql"
+        });
+
+        config.reset();
+
+        expect(config.get("app.environment")).to.equal("test");
+        expect(config.get("app.timezone")).to.equal("UTC");
+        expect(config.get("database")).to.deep.equal(defaultDbConfig);
+      });
+
+      it("should reset only specified `path` if exists", function() {
+        expect(config.get("app.environment")).to.equal("test");
+        expect(config.get("app.timezone")).to.equal("UTC");
+        expect(config.get("database")).to.deep.equal(defaultDbConfig);
+
+        config.set("app.environment", "staging");
+        config.set("app.timezone", "Africa/Lagos");
+        config.set("database.default", "mysql");
+
+        expect(config.get("app.environment")).to.equal("staging");
+        expect(config.get("app.timezone")).to.equal("Africa/Lagos");
+        expect(config.get("database")).to.deep.equal({
+          ...defaultDbConfig, default: "mysql"
+        });
+
+        config.reset("app.timezone");
+
+        expect(config.get("app.timezone")).to.equal("UTC");
+        expect(config.get("app.environment")).to.equal("staging");
+        expect(config.get("database")).to.deep.equal({
+          ...defaultDbConfig, default: "mysql"
+        });
+
+        config.set("app.timezone", "Africa/Lagos");
+
+        expect(config.get("app.environment")).to.equal("staging");
+        expect(config.get("app.timezone")).to.equal("Africa/Lagos");
+
+        config.reset("app");
+
+        expect(config.get("app.environment")).to.equal("test");
+        expect(config.get("app.timezone")).to.equal("UTC");
+        expect(config.get("database")).to.deep.equal({
+          ...defaultDbConfig, default: "mysql"
+        });
+
+        config.reset("database.default");
+
+        expect(config.get("database")).to.deep.equal(defaultDbConfig);
       });
     });
   },
