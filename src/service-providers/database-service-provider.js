06fc394b61aecadab9168cf941286595b2872c50
simplymichael <simplymichaelorji@gmail.com>
2024-06-20 00:00:09 +0100

    refactor: implement a database service provider

---
+++ src/service-providers/database-service-provider.js
@@ -0,0 +1,26 @@
+const DatabaseFactory = require("../framework/factory/database");
+const ServiceProvider = require("./service-provider");
+
+
+module.exports = class DatabaseServiceProvider extends ServiceProvider {
+  constructor(config) {
+    super(config);
+  }
+
+  register() {
+    const container = this.container();
+    const config = this.config() ?? container.resolve("config");
+    const dbConfig = config.get("database");
+    const driver = dbConfig.default;
+    const driverConfig = dbConfig.connections[driver];
+
+    const db = DatabaseFactory.createDatastore(driver, driverConfig);
+
+    /*
+     * Bind the default database to the container
+     */
+    container.bind("db", function createDatastore() {
+      return db;
+    });
+  }
+};
