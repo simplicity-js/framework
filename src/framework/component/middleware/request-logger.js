561ab2d3555fff0f062c956ed836a34f332ba91b
simplymichael <simplymichaelorji@gmail.com>
2024-06-21 22:27:36 +0100

    feat: implement a framework-level http requests logger middleware

---
+++ src/framework/component/middleware/request-logger.js
@@ -0,0 +1,41 @@
+const morgan = require("morgan");
+
+
+module.exports = function logHttpRequests(app) {
+  let loggerMiddleware;
+  const logger = app.resolve("logger") || console;
+
+  /*
+   * If the logger is using log levels different from winston.config.npm.levels,
+   * we may not have the logger.http method. In that case,
+   * we fallback to using morgan('tiny') which is an alias for
+   * morgan(":method :url :status :res[content-length] - :response-time ms").
+   */
+  if(typeof logger?.http !== "function") {
+    loggerMiddleware = morgan("tiny");
+  } else {
+    const config = {
+      stream: {
+        /*
+         * Configure Morgan to use our custom (winston)
+         * logger with the http severity
+         */
+        write: (message) => logger.http("incoming-request", JSON.parse(message)),
+      },
+    };
+
+    loggerMiddleware = morgan(formatter, config);
+  }
+
+  return loggerMiddleware;
+};
+
+function formatter(tokens, req, res) {
+  return JSON.stringify({
+    method         : tokens.method(req, res),
+    url            : tokens.url(req, res),
+    status         : Number.parseFloat(tokens.status(req, res)),
+    content_length : tokens.res(req, res, "content-length"),
+    response_time  : Number.parseFloat(tokens["response-time"](req, res)),
+  });
+}
